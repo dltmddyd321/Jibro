3f5f2899ae941fc11056844b43e67dfbd319104d
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2025-07-01 21:34:45 +0900

    근처 역 좌표 구하기
    - 위도 경도 계산을 통해 가장 가까운 거리의 역 1개 반환 처리

--- jibrro/app/src/main/java/com/windrr/jibrro/presentation/viewmodel/StationViewModel.kt
+++ jibrro/app/src/main/java/com/windrr/jibrro/presentation/viewmodel/StationViewModel.kt
@@ -7,6 +7,11 @@ import com.windrr.jibrro.data.model.SubwayStation
 import com.windrr.jibrro.domain.usecase.GetStationListUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
+import kotlin.math.atan2
+import kotlin.math.cos
+import kotlin.math.pow
+import kotlin.math.sin
+import kotlin.math.sqrt
 
 @HiltViewModel
 class StationViewModel @Inject constructor(
@@ -22,5 +27,25 @@ class StationViewModel @Inject constructor(
 
     private fun loadStations() {
         _stations.value = getSubwayStationsUseCase()
+    }
+
+    fun findClosestStation(currentLat: Double, currentLon: Double): SubwayStation? {
+        return _stations.value?.minByOrNull {
+            distanceInMeters(currentLat, currentLon, it.lat, it.lng)
+        }
+    }
+
+    private fun distanceInMeters(
+        lat1: Double, lon1: Double,
+        lat2: Double, lon2: Double
+    ): Double {
+        val R = 6371000.0 // Earth radius in meters
+        val dLat = Math.toRadians(lat2 - lat1)
+        val dLon = Math.toRadians(lon2 - lon1)
+        val a = sin(dLat / 2).pow(2.0) +
+                cos(Math.toRadians(lat1)) * cos(Math.toRadians(lat2)) *
+                sin(dLon / 2).pow(2.0)
+        val c = 2 * atan2(sqrt(a), sqrt(1 - a))
+        return R * c
     }
 }
