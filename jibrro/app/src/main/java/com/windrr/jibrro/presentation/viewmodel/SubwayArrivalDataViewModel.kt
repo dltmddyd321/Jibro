e66c7574ccdc4af4d6db67976bedfa7183018f3d
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2025-07-06 21:34:13 +0900

    ViewModel 구성 진행

--- jibrro/app/src/main/java/com/windrr/jibrro/presentation/viewmodel/SubwayArrivalDataViewModel.kt
+++ jibrro/app/src/main/java/com/windrr/jibrro/presentation/viewmodel/SubwayArrivalDataViewModel.kt
@@ -1,12 +1,29 @@
 package com.windrr.jibrro.presentation.viewmodel
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.windrr.jibrro.data.model.SubwayArrival
 import com.windrr.jibrro.domain.usecase.GetSubwayArrivalDataUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
+import com.windrr.jibrro.data.util.Result
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 
 @HiltViewModel
 class SubwayArrivalDataViewModel @Inject constructor(
     private val getSubwayArrivalDataUseCase: GetSubwayArrivalDataUseCase
-) {
-    
+) : ViewModel() {
+    private val _arrivalState = MutableStateFlow<Result<List<SubwayArrival>>>(Result.Loading())
+    val arrivalState: StateFlow<Result<List<SubwayArrival>>> = _arrivalState.asStateFlow()
+
+    fun getSubwayArrival(statnNm: String) {
+        viewModelScope.launch {
+            _arrivalState.value = Result.Loading()
+            val result = getSubwayArrivalDataUseCase.execute(statnNm = statnNm)
+            _arrivalState.value = result
+        }
+    }
 }
