0a2c53679bcb455528e3b8169b54b0b4056ab158
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2025-07-13 21:00:13 +0900

    체크 박스 버튼 추가

--- jibrro/app/src/main/java/com/windrr/jibrro/presentation/activity/LikeStationActivity.kt
+++ jibrro/app/src/main/java/com/windrr/jibrro/presentation/activity/LikeStationActivity.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -19,19 +22,34 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.windrr.jibrro.presentation.activity.ui.theme.JibrroTheme
+import com.windrr.jibrro.presentation.viewmodel.StationViewModel
+import com.windrr.jibrro.presentation.viewmodel.SubwayArrivalDataViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class LikeStationActivity : ComponentActivity() {
+    private val stationViewModel: StationViewModel by viewModels()
     @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        stationViewModel.fetchStationList()
+
         enableEdgeToEdge()
         setContent {
             JibrroTheme {
+
+                val stationList by stationViewModel.allStation.collectAsState()
+                val isLoading by stationViewModel.isLoading.collectAsState()
+
                 Scaffold(
                     modifier = Modifier.fillMaxSize(),
                     topBar = {
@@ -83,22 +101,34 @@ fun GreetingPreview() {
 fun StationListItem(
     name: String,
     line: String,
+    checked: Boolean,
+    onCheckedChange: (Boolean) -> Unit,
     modifier: Modifier = Modifier
 ) {
-    Column(
+    Row(
         modifier = modifier
             .fillMaxWidth()
-            .padding(vertical = 8.dp, horizontal = 16.dp)
+            .padding(vertical = 8.dp, horizontal = 16.dp),
+        verticalAlignment = Alignment.CenterVertically
     ) {
-        Text(
-            text = name,
-            color = Color.Black,
-            style = MaterialTheme.typography.bodyLarge
-        )
-        Text(
-            text = line,
-            color = Color.Red,
-            style = MaterialTheme.typography.bodySmall
+        Column(
+            modifier = Modifier.weight(1f)
+        ) {
+            Text(
+                text = name,
+                color = Color.Black,
+                style = MaterialTheme.typography.bodyLarge
+            )
+            Text(
+                text = line,
+                color = Color.Red,
+                style = MaterialTheme.typography.bodySmall
+            )
+        }
+
+        Checkbox(
+            checked = checked,
+            onCheckedChange = onCheckedChange
         )
     }
 }
