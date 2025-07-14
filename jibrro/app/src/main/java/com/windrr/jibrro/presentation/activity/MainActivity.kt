53749024876e3d56800b9cbb8ab3a26342a91b58
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2025-07-14 21:26:39 +0900

    드로어블 레이아웃 구성
    - 설정 및 즐겨찾기 메뉴 버튼 추가

--- jibrro/app/src/main/java/com/windrr/jibrro/presentation/activity/MainActivity.kt
+++ jibrro/app/src/main/java/com/windrr/jibrro/presentation/activity/MainActivity.kt
@@ -1,24 +1,43 @@
 package com.windrr.jibrro.presentation.activity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.DismissibleNavigationDrawer
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import com.windrr.jibrro.presentation.ui.theme.JibrroTheme
 import com.windrr.jibrro.presentation.viewmodel.StationViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -41,22 +60,64 @@ class MainActivity : ComponentActivity() {
                 val stationName by stationViewModel.closestStation.collectAsState()
                 val isLoading by stationViewModel.isLoading.collectAsState()
 
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Box(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(innerPadding),
-                        contentAlignment = Alignment.Center
-                    ) {
-                        if (isLoading) {
-                            CircularProgressIndicator()
-                        } else {
-                            Text(
-                                text = stationName ?: "지하철역을 찾을 수 없습니다.",
-                                style = MaterialTheme.typography.titleLarge
-                            )
-                        }
+                MainDrawerScreen {
+                    if (isLoading) {
+                        CircularProgressIndicator()
+                    } else {
+                        Text(
+                            text = stationName ?: "지하철역을 찾을 수 없습니다.",
+                            style = MaterialTheme.typography.titleLarge
+                        )
                     }
+                }
+            }
+        }
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun MainDrawerScreen(
+        content: @Composable () -> Unit
+    ) {
+        val drawerState = rememberDrawerState(DrawerValue.Closed)
+        val scope = rememberCoroutineScope()
+        DismissibleNavigationDrawer(
+            drawerState = drawerState, drawerContent = {
+                ModalDrawerSheet {
+                    Text(
+                        "즐겨찾기", modifier = Modifier
+                            .fillMaxWidth()
+                            .clickable {
+                                scope.launch { drawerState.close() }
+                                startActivity(Intent(this@MainActivity, LikeStationActivity::class.java))
+                            }
+                            .padding(16.dp))
+                    Text(
+                        "설정", modifier = Modifier
+                            .fillMaxWidth()
+                            .clickable {
+                                scope.launch { drawerState.close() }
+                            }
+                            .padding(16.dp))
+                }
+            }) {
+            Scaffold(
+                topBar = {
+                    TopAppBar(title = { Text("드로어 예제") }, navigationIcon = {
+                        IconButton(onClick = {
+                            scope.launch { drawerState.open() }
+                        }) {
+                            Icon(Icons.Filled.Menu, contentDescription = "메뉴 열기")
+                        }
+                    })
+                }) { innerPadding ->
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(innerPadding),
+                    contentAlignment = Alignment.Center
+                ) {
+                    content()
                 }
             }
         }
