385c51d18ad0ac3ee769d2908bd996230ba439fd
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2025-07-29 21:27:45 +0900

    광고 로드에 성공했을 때에만 배너 광고 보이도록 처리

--- jibrro/app/src/main/java/com/windrr/jibrro/presentation/component/BannerAdView.kt
+++ jibrro/app/src/main/java/com/windrr/jibrro/presentation/component/BannerAdView.kt
@@ -1,29 +1,49 @@
 package com.windrr.jibrro.presentation.component
 
+import android.util.Log
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
+import com.google.android.gms.ads.AdListener
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdSize
 import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.LoadAdError
 
 @Composable
 fun BannerAdView(
     modifier: Modifier = Modifier
 ) {
-    AndroidView(
-        modifier = modifier
-            .fillMaxWidth()
-            .height(50.dp),
-        factory = { context ->
-            AdView(context).apply {
-                setAdSize(AdSize.BANNER)
-                adUnitId = "ca-app-pub-2156013754929909/4254063400"
-                loadAd(AdRequest.Builder().build())
+    var isAdVisible by remember { mutableStateOf(false) }
+    if (isAdVisible) {
+        AndroidView(
+            modifier = modifier
+                .fillMaxWidth()
+                .height(50.dp),
+            factory = { context ->
+                AdView(context).apply {
+                    setAdSize(AdSize.BANNER)
+                    adUnitId = "ca-app-pub-2156013754929909/4254063400"
+                    adListener = object : AdListener() {
+                        override fun onAdLoaded() {
+                            isAdVisible = true
+                        }
+
+                        override fun onAdFailedToLoad(error: LoadAdError) {
+                            isAdVisible = false
+                            Log.e("AdView", "Ad failed: ${error.message}")
+                        }
+                    }
+                    loadAd(AdRequest.Builder().build())
+                }
             }
-        }
-    )
+        )
+    }
 }
