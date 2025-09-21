c491f70450987719f6d2a8cac6d6380891cf381a
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2025-09-21 14:44:12 +0900

    버전 업데이트 (2)

--- jibrro/app/build.gradle.kts
+++ jibrro/app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
         applicationId = "com.windrr.jibrro"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
