18c2c45927a316c579ec6931c181da101b2ee4ba
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2025-09-27 20:34:15 +0900

    Update build.gradle.kts

--- jibrro/app/build.gradle.kts
+++ jibrro/app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
         applicationId = "com.windrr.jibrro"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
