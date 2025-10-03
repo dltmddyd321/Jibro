72ba7e0d7f1923587418362964739a24f4708d55
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2025-10-04 00:07:26 +0900

    UseCase가 UseCase를 주입받는 구조 수정

--- jibrro/app/src/main/java/com/windrr/jibrro/domain/usecase/GetClosestStationUseCase.kt
+++ jibrro/app/src/main/java/com/windrr/jibrro/domain/usecase/GetClosestStationUseCase.kt
@@ -1,14 +1,15 @@
 package com.windrr.jibrro.domain.usecase
 
 import com.windrr.jibrro.data.model.SubwayStation
+import com.windrr.jibrro.domain.repository.StationRepository
 import com.windrr.jibrro.util.distanceInMeters
 import javax.inject.Inject
 
 class GetClosestStationUseCase @Inject constructor(
-    private val getStationListUseCase: GetStationListUseCase
+    private val stationRepository: StationRepository
 ) {
     operator fun invoke(lat: Double, lng: Double): SubwayStation? {
-        val stations = getStationListUseCase()
+        val stations = stationRepository.getSubwayStations()
             .filter { it.lat != 0.0 && it.lng != 0.0 }
 
         return stations.minByOrNull { station ->
