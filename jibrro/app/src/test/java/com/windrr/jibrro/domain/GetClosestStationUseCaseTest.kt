dd164d6e9a4e74fca8bf7fe79c13cd031d45396f
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2025-10-04 22:38:23 +0900

    가장 가까운 지하철 역 가져오기 테스트

--- jibrro/app/src/test/java/com/windrr/jibrro/domain/GetClosestStationUseCaseTest.kt
+++ jibrro/app/src/test/java/com/windrr/jibrro/domain/GetClosestStationUseCaseTest.kt
@@ -4,6 +4,9 @@ import com.windrr.jibrro.data.model.SubwayStation
 import com.windrr.jibrro.domain.repository.StationRepository
 import com.windrr.jibrro.domain.usecase.GetClosestStationUseCase
 import org.junit.Before
+import org.junit.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
 
 class GetClosestStationUseCaseTest {
     inner class FakeStationRepository : StationRepository {
@@ -48,6 +51,45 @@ class GetClosestStationUseCaseTest {
         stationsToInsert.forEach {
             fakeStationRepository.addStation(it)
         }
+    }
+
+    @Test
+    fun `returns one nearest station`() {
+        val userLat = 37.5665
+        val userLng = 126.9780
+
+        val expected = SubwayStation(
+            line = "L1",
+            name = "Station ZERO",
+            station_nm_chn = "站ZERO",
+            station_nm_jpn = "ステーションZERO",
+            station_nm_eng = "Station ZERO",
+            fr_code = "01-00",
+            station_cd = "9999",
+            bldn_id = "B19999",
+            lat = userLat,
+            lng = userLng
+        )
+
+        val sub = SubwayStation(
+            line = "L2",
+            name = "Sub Station",
+            station_nm_chn = "Sub Station",
+            station_nm_jpn = "Sub Station",
+            station_nm_eng = "Sub Station",
+            fr_code = "01-01",
+            station_cd = "9999",
+            bldn_id = "B19999",
+            lat = 57.5474,
+            lng = 129.6453
+        )
+        fakeStationRepository.addStation(expected)
+        fakeStationRepository.addStation(sub)
+
+        val result = getClosestStationUseCase(userLat, userLng)
+
+        assertNotNull(result, "가장 가까운 역이 null이면 안 됩니다.")
+        assertEquals(expected.line, result.line, "가장 가까운 역이 기대한 역과 일치해야 합니다.")
     }
 }
 
